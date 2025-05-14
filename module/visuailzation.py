bdf220dc8ad85954b00e578705e41bc4aa807275
JunwooParkSaribu <junwoo@junwoo.>
2025-05-14 16:19:00 +0200

    updated

--- module/visuailzation.py
+++ module/visuailzation.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import matplotlib.colors as mcolors
 import matplotlib.patches as mpatches
+import matplotlib as mpl
 from tqdm import tqdm
 
 
@@ -74,6 +75,105 @@ def trajectory_visualization(original_data:pd.DataFrame, analysis_data1:pd.DataF
     color_maps_plot['transitioning'] = 'yellow'
     patches =[mpatches.Patch(color=c,label=color_maps[c]) for c in color_maps]
     return image, patches, color_maps, color_maps_plot
+
+
+def quantification(window_size):
+    x = np.arange(-(window_size-1)/2, (window_size+1)/2)
+    y = np.arange(-(window_size-1)/2, (window_size+1)/2)
+    xv, yv = np.meshgrid(x, y, sparse=True)
+    grid = np.stack(np.meshgrid(xv, yv), -1).reshape(window_size * window_size, 2)
+    return grid.astype(np.float32)
+
+
+def trajectory_visualization_tmp(original_data:pd.DataFrame, analysis_data1:pd.DataFrame, cutoff:int, pixelmicron:float, resolution_multiplier=20) -> np.ndarray:
+    print("** visualizing trajectories... **")
+    import matplotlib.pyplot as plt
+    from PIL import Image
+    scale = resolution_multiplier
+    thickness = 1
+    color_maps = {}
+    color_maps_plot = {}
+    winsize = 55
+    cov_std = 55
+    margin_pixel = 200
+
+    min_x = original_data['x'].min()
+    min_y = original_data['y'].min()
+    max_x = original_data['x'].max()
+    max_y = original_data['y'].max()
+    x_width = int(((max_x - min_x) * scale) + margin_pixel)
+    y_width = int(((max_y - min_y) * scale) + margin_pixel)
+    image = np.ones((y_width, x_width)).astype(np.float64)
+
+
+    mycmap = plt.get_cmap('magma', lut=None)
+    color_seq = [mycmap(i)[:3] for i in range(mycmap.N)]
+    all_coords = np.vstack([original_data['x'].to_numpy(), original_data['y'].to_numpy()]).T
+    all_coords[:,0] = all_coords[:,0] - min_x
+    all_coords[:,1] = all_coords[:,1] - min_y
+    all_coords = np.round(all_coords * scale)
+
+    template = np.ones((1, (winsize)**2, 2), dtype=np.float32) * quantification(winsize)
+    template = (np.exp((-1./2) * np.sum(template @ np.linalg.inv([[cov_std, 0], [0, cov_std]]) * template, axis=2))).reshape([winsize, winsize])
+
+    for roundup_coord in all_coords:
+        coord_col = int(roundup_coord[0] + margin_pixel//2)
+        coord_row = int(roundup_coord[1] + margin_pixel//2)
+        row = min(max(0, coord_row), image.shape[0])
+        col = min(max(0, coord_col), image.shape[1])
+        try:
+            image[row - winsize//2: row + winsize//2 + 1, col - winsize//2: col + winsize//2 + 1] += template
+        except:
+            pass
+    
+    img_min, img_max = np.quantile(image, [0.01, 1.0])
+    image = np.minimum(image, np.ones_like(image) * img_max)
+    image = image / np.max(image)
+    image = (image * 255).astype(np.uint8)
+    image = plt.imshow(image, cmap=mycmap, origin='upper')
+    image = image.make_image(renderer=None, unsampled=True)[0][:,:,:3]
+    image = np.array(Image.fromarray(image))
+    image = np.array(image).astype(np.uint8)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGRA)
+    print(image.shape)
+    #image.save(f'{output_dir}_loc_{dim}d_density.png', dpi=(300, 300))
+
+    
+    n_lines = 100
+    cmap = mpl.colormaps['jet']
+    colors = cmap(np.linspace(0, 1, n_lines))[::-1][0:75]
+
+    for traj_idx in tqdm(original_data['traj_idx'].unique(), ncols=120, desc=f'Visualization', unit='trajectory'):
+        single_traj = original_data[original_data['traj_idx'] == traj_idx]
+        corresponding_ids = analysis_data1['traj_id'] == single_traj['traj_idx'].iloc[0]
+        tmpx = single_traj['x'].to_numpy() 
+        tmpy = single_traj['y'].to_numpy()
+        dips = np.mean(np.sqrt((tmpx[1:] - tmpx[:-1])**2 + (tmpy[1:] - tmpy[:-1])**2))
+        if np.sum(corresponding_ids) > 0:
+            traj_color = analysis_data1[analysis_data1['traj_id'] == single_traj['traj_idx'].iloc[0]]['color'].iloc[0]
+            state = analysis_data1[analysis_data1['traj_id'] == single_traj['traj_idx'].iloc[0]]['state'].iloc[0]
+            if traj_color not in color_maps and traj_color != 'yellow':
+                color_maps[traj_color] = str(state)
+                color_maps_plot[state] = traj_color
+            if len(single_traj) >= cutoff:
+                traj_color = mcolors.to_rgb(traj_color)
+                traj_color = (int(traj_color[2]*255), int(traj_color[1]*255), int(traj_color[0]*255))  # BGR color for cv2
+                pts = np.array([[int((x - min_x) * scale) + margin_pixel//2, int((y - min_y) * scale) + margin_pixel//2] for x, y in zip(single_traj['x'], single_traj['y'])], np.int32)
+                for i in range(len(pts)-1):
+                    prev_pt = pts[i]
+                    next_pt = pts[i+1]
+                    disp_ = np.sum((pts[i+1]/scale - pts[i]/scale)**2)
+                    if disp_ < 0.4:
+                        mymax = 0.70
+                        color_rgba = colors[int(min(1, (dips / mymax)) * (len(colors)-1))]
+                        target_color = (int(color_rgba[2]*255), int(color_rgba[1]*255), int(color_rgba[0]*255))
+                        cv2.line(image, prev_pt, next_pt, target_color, thickness)
+    cv2.line(image, [int(max(0, x_width - 5*scale - int(scale/pixelmicron))), int(max(0, y_width - 5*scale))], [int(max(0, x_width - 5*scale)) , int(max(0, y_width - 5*scale))], (255, 255, 255), 6)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    color_maps['yellow'] = 'transitioning'
+    color_maps_plot['transitioning'] = 'yellow'
+    patches =[mpatches.Patch(color=c,label=color_maps[c]) for c in color_maps]
+    return image.copy(), patches, color_maps, color_maps_plot
 
 
 def draw_labeled_multigraph(G, attr_names, cmap=None, ax=None):
