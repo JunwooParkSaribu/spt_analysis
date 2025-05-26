fecf6bf88fc94143289d18170a9f3bf121bf9180
JunwooParkSaribu <junwoo@junwoo.>
2025-05-26 19:54:13 +0200

    updated

--- analysis_conditions.py
+++ analysis_conditions.py
@@ -1,9 +1,11 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.lines import Line2D
 from module.preprocessing import preprocessing
 from module.fileIO.DataLoad import read_multiple_csv, read_multiple_h5s
-from scipy.stats import bootstrap
+from scipy.stats import bootstrap, ks_2samp, ecdf
 import numpy as np
 
 
@@ -27,16 +29,19 @@ We concatenate different conditions of Dataframe into one for further analysis.
 """
 analysis_data1 = pd.DataFrame({})
 analysis_data2 = pd.DataFrame({})
+analysis_data3 = pd.DataFrame({})
 tamsds = pd.DataFrame({})
 state_markovs = []
 state_graphs = []
 for condition in CONDITIONS:
     data = read_multiple_h5s(path=condition)
-    data1, data2, state_markov, state_graph, msd, tamsd, states, state_changing_duration = preprocessing(data=data, pixelmicrons=PIXELMICRONS, framerate=FRAMERATE, cutoff=CUTOFF, tamsd_calcul=False)
+    data1, data2, data3, state_markov, state_graph, msd, tamsd, states, state_changing_duration = preprocessing(data=data, pixelmicrons=PIXELMICRONS, framerate=FRAMERATE, cutoff=CUTOFF, tamsd_calcul=False)
     data1['condition'] = [condition] * len(data1)
     data2['condition'] = [condition] * len(data2)
+    data3['condition'] = [condition] * len(data3)
     analysis_data1 = pd.concat([analysis_data1, data1], ignore_index=True)
     analysis_data2 = pd.concat([analysis_data2, data2], ignore_index=True)
+    analysis_data3 = pd.concat([analysis_data3, data3], ignore_index=True)
     state_markovs.append(state_markov)
     state_graphs.append(state_graph)
     if tamsd is not None:
@@ -51,11 +56,14 @@ From here, we treat the data to make plots or print results.
 Data is stored as
 1.analysis_data1: (DataFrame: contains data of mean_jump_distance, log10_K, alpha, state, duration, traj_id, condition)
 2.analysis_data2: (DataFrame: contains data of displacments, state, condition)
-3.state_markovs: (matrix: contains transition probability of conditions)
-4.state_graphs: (network: built from transitions between states (count: nb of occurence of transitions) of conditions)
+3.analysis_data3: (DataFrame: contains data of angles, state, condition)
+4.state_markovs: (matrix: contains transition probability of conditions)
+5.state_graphs: (network: built from transitions between states (count: nb of occurence of transitions) of conditions)
 """
 print(f'\nanalysis_data1:\n', analysis_data1)
 print(f'\nanalysis_data2:\n', analysis_data2)
+print(f'\nanalysis_data3:\n', analysis_data3)
+
 
 
 #p1: histogram with kde(kernel density estimation) plot of mean jump distance grouped by state.
@@ -132,6 +140,37 @@ if tamsds is not None:
     plt.xticks(fontsize=figure_font_size)
     plt.xticks(rotation=90)
     plt.tight_layout()
+
+
+#p6: angles histogram
+fig, axs = plt.subplots(1, 2, num=f'p6', figsize=(18, 9))
+sns.histplot(data=analysis_data3, x='angles', stat='proportion', hue='condition', common_norm=False, bins=number_of_bins, kde=True, ax=axs[0], kde_kws={'bw_adjust': 1})
+sns.ecdfplot(data=analysis_data3, x='angles', stat='proportion', hue='condition', ax=axs[1])
+axs[0].set_title(f'angle histogram')
+axs[0].set_xlabel(r'clock-wise angle (degree)')
+axs[1].set_title(f'angle CDF')
+axs[1].set_xlabel(r'clock-wise angle (degree)')
+cmap = mpl.colormaps['cividis']
+custom_lines = [Line2D([0], [0], color=cmap(i/(len(CONDITIONS) - 1)), lw=2) for i in range(len(CONDITIONS) - 1)]
+legend_labels = []
+legend_results = []
+for idx in range(1, len(CONDITIONS)):
+    gt = analysis_data3[analysis_data3['condition']==CONDITIONS[0]]['angles']
+    comp = analysis_data3[analysis_data3['condition']==CONDITIONS[idx]]['angles']
+    ecdf_comp = ecdf(comp)
+    ecdf_gt = ecdf(gt)
+    result = ks_2samp(gt, comp, method='exact')
+    axs[1].vlines(result.statistic_location, ecdf_comp.cdf.evaluate(result.statistic_location), ecdf_gt.cdf.evaluate(result.statistic_location), colors=cmap((idx-1)/(len(CONDITIONS) - 1)), alpha=0.6)
+    legend_labels.append(f'D: {np.round(result.statistic, 3)}, ground_truth v {CONDITIONS[idx]}')
+    legend_results.append(np.round(result.statistic, 3))
+custom_lines = np.array(custom_lines)[np.argsort(legend_results)]
+legend_labels = np.array(legend_labels)[np.argsort(legend_results)]
+old_legend = axs[0].legend_
+handles = old_legend.legend_handles
+labels = [t.get_text() for t in old_legend.get_texts()]
+axs[0].legend(handles, labels)
+axs[1].legend(custom_lines, legend_labels, title='KS test')
+plt.tight_layout()
 
 
 plt.show()
